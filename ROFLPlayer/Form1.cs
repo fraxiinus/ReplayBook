2d0be3193c26250798c3405308f2e82dd0d6b09f
andre <andre@MIDNA>
2017-06-18 20:40:25 -0400

    Add project files.

---
+++ ROFLPlayer/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Diagnostics;
+
+namespace ROFLPlayer
+{
+    public partial class Form1 : Form
+    {
+
+        private string LoLExecFile = "";
+        private string ReplayFile = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxLoLPath_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files != null && files.Length != 0)
+            {
+                textBoxLoLPath.Text = files[0];
+            }
+        }
+
+        private void textBoxLoLPath_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false) == true)
+            {
+                e.Effect = DragDropEffects.All;
+            }
+        }
+
+        private void textBoxLolPath_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxLoLPath.Text = "Browse for LoL game executable...";
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            var FD = new OpenFileDialog();
+            FD.Filter = "Executable files (*.exe)|*.exe";
+            if (FD.ShowDialog() == DialogResult.OK)
+            {
+                textBoxLoLPath.Text = FD.FileName;
+            }
+        }
+
+        private void textBoxLoLPath_TextChanged(object sender, EventArgs e)
+        {
+            if(Regex.IsMatch(textBoxLoLPath.Text, @"League of Legends.exe"))
+            {
+                labelValid.Text = "O";
+                labelValid.ForeColor = Color.Green;
+                LoLExecFile = textBoxLoLPath.Text;
+                if(ReplayFile != "")
+                {
+                    buttonPlay.Enabled = true;
+                }
+            }
+            else
+            {
+                labelValid.Text = "X";
+                labelValid.ForeColor = Color.Red;
+                LoLExecFile = "";
+                buttonPlay.Enabled = false;
+            }
+        }
+
+        private void splitsplitContainer1_Panel2_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files != null && files.Length != 0)
+            {
+                if(Path.GetExtension(files[0]) != ".rofl")
+                {
+                    MessageBox.Show("Invalid file type", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    ReplayFile = "";
+                    buttonPlay.Enabled = false;
+                }
+                else
+                {
+                    ReplayFile = files[0];
+                    label2.Text = Path.GetFileName(ReplayFile);
+                    if(LoLExecFile != "")
+                    {
+                        buttonPlay.Enabled = true;
+                    }
+                }
+            }
+        }
+
+        private void splitsplitContainer1_Panel2_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false) == true)
+            {
+                e.Effect = DragDropEffects.All;
+            }
+        }
+
+        private void splitsplitContainer1_Panel2_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            buttonPlay.Enabled = false;
+            label2.Text = "Copying...";
+            string newpath = Path.GetDirectoryName(LoLExecFile) + "\\" + Path.GetFileName(ReplayFile);
+            File.Copy(ReplayFile, newpath, true);
+            label2.Text = "Playing...";
+            var proc = new Process();
+
+            proc.StartInfo.FileName = LoLExecFile;
+            proc.StartInfo.Arguments = Path.GetFileName(newpath);
+            proc.StartInfo.WorkingDirectory = Path.GetDirectoryName(LoLExecFile);
+            proc.Start();
+            while (!proc.HasExited) { }
+            label2.Text = "Drag Replays Here";
+        }
+    }
+}
---
+++ ROFLPlayer/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ROFLPlayer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ROFLPlayer")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8ea3d1b0-88a4-4909-89a4-788fde0d977d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
