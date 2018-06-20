6c64faf690317125356d0cb4ea9b412fdb966194
Andrew Lee <andrew1421lee@gmail.com>
2018-06-20 19:58:58 -0400

    Added file manager, creates shortcuts

---
+++ ROFLPlayer/Lib/FileManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IWshRuntimeLibrary;
+using System.IO;
+
+namespace ROFLPlayer.Lib
+{
+    public class FileManager
+    {
+
+        // Creates shortcuts, modifies shortcuts if exists
+        public static IWshShortcut CreateShortcut(string shortcutpath, string execpath, string replaypath)
+        {
+            var shell = new WshShell();
+            var shortcut = (IWshShortcut)shell.CreateShortcut(shortcutpath);
+
+            shortcut.Description = "ROFL Player replay shortcut";
+            shortcut.TargetPath = execpath;
+            shortcut.WorkingDirectory = Path.GetDirectoryName(shortcutpath);
+            shortcut.Arguments = "\"" + replaypath + "\"";
+            shortcut.Save();
+
+            return shortcut;
+        }
+    }
+}
