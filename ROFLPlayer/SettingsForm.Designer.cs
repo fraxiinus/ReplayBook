efbfe78344256eb3cded04822df6b8419acf5d9f
Andrew Lee <andrew1421lee@gmail.com>
2018-06-20 00:15:28 -0400

    update control names

--- ROFLPlayer/SettingsForm.Designer.cs
+++ ROFLPlayer/SettingsForm.Designer.cs
@@ -29,46 +29,49 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SettingsForm));
-            this.settingsTabControl = new System.Windows.Forms.TabControl();
-            this.MainTab = new System.Windows.Forms.TabPage();
+            this.MainTabControl = new System.Windows.Forms.TabControl();
+            this.GeneralTab = new System.Windows.Forms.TabPage();
             this.AboutTab = new System.Windows.Forms.TabPage();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.GeneralFlowLayout = new System.Windows.Forms.FlowLayoutPanel();
             this.GeneralGameLabel = new System.Windows.Forms.Label();
             this.GeneralGameTextBox = new System.Windows.Forms.TextBox();
             this.GeneralGameBrowseButton = new System.Windows.Forms.Button();
             this.MainPlayButton = new System.Windows.Forms.Button();
             this.MainCancelButton = new System.Windows.Forms.Button();
             this.MainOkButton = new System.Windows.Forms.Button();
-            this.settingsTabControl.SuspendLayout();
-            this.MainTab.SuspendLayout();
-            this.flowLayoutPanel1.SuspendLayout();
+            this.AboutFlowLayout = new System.Windows.Forms.FlowLayoutPanel();
+            this.MainTabControl.SuspendLayout();
+            this.GeneralTab.SuspendLayout();
+            this.AboutTab.SuspendLayout();
+            this.GeneralFlowLayout.SuspendLayout();
             this.SuspendLayout();
             // 
-            // settingsTabControl
+            // MainTabControl
             // 
-            this.settingsTabControl.Controls.Add(this.MainTab);
-            this.settingsTabControl.Controls.Add(this.AboutTab);
-            this.settingsTabControl.Dock = System.Windows.Forms.DockStyle.Top;
-            this.settingsTabControl.Location = new System.Drawing.Point(5, 5);
-            this.settingsTabControl.Name = "settingsTabControl";
-            this.settingsTabControl.SelectedIndex = 0;
-            this.settingsTabControl.Size = new System.Drawing.Size(348, 408);
-            this.settingsTabControl.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
-            this.settingsTabControl.TabIndex = 0;
+            this.MainTabControl.Controls.Add(this.GeneralTab);
+            this.MainTabControl.Controls.Add(this.AboutTab);
+            this.MainTabControl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.MainTabControl.Location = new System.Drawing.Point(5, 5);
+            this.MainTabControl.Name = "MainTabControl";
+            this.MainTabControl.SelectedIndex = 0;
+            this.MainTabControl.Size = new System.Drawing.Size(348, 408);
+            this.MainTabControl.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.MainTabControl.TabIndex = 0;
             // 
-            // MainTab
+            // GeneralTab
             // 
-            this.MainTab.Controls.Add(this.flowLayoutPanel1);
-            this.MainTab.Location = new System.Drawing.Point(4, 22);
-            this.MainTab.Name = "MainTab";
-            this.MainTab.Padding = new System.Windows.Forms.Padding(3);
-            this.MainTab.Size = new System.Drawing.Size(340, 382);
-            this.MainTab.TabIndex = 1;
-            this.MainTab.Text = "General";
-            this.MainTab.UseVisualStyleBackColor = true;
+            this.GeneralTab.Controls.Add(this.GeneralFlowLayout);
+            this.GeneralTab.Location = new System.Drawing.Point(4, 22);
+            this.GeneralTab.Name = "GeneralTab";
+            this.GeneralTab.Padding = new System.Windows.Forms.Padding(3);
+            this.GeneralTab.Size = new System.Drawing.Size(340, 382);
+            this.GeneralTab.TabIndex = 1;
+            this.GeneralTab.Text = "General";
+            this.GeneralTab.UseVisualStyleBackColor = true;
             // 
             // AboutTab
             // 
+            this.AboutTab.Controls.Add(this.AboutFlowLayout);
             this.AboutTab.Location = new System.Drawing.Point(4, 22);
             this.AboutTab.Name = "AboutTab";
             this.AboutTab.Padding = new System.Windows.Forms.Padding(3);
@@ -77,17 +80,17 @@
             this.AboutTab.Text = "About";
             this.AboutTab.UseVisualStyleBackColor = true;
             // 
-            // flowLayoutPanel1
+            // GeneralFlowLayout
             // 
-            this.flowLayoutPanel1.Controls.Add(this.GeneralGameLabel);
-            this.flowLayoutPanel1.Controls.Add(this.GeneralGameTextBox);
-            this.flowLayoutPanel1.Controls.Add(this.GeneralGameBrowseButton);
-            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(0, 5, 0, 5);
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(334, 376);
-            this.flowLayoutPanel1.TabIndex = 0;
+            this.GeneralFlowLayout.Controls.Add(this.GeneralGameLabel);
+            this.GeneralFlowLayout.Controls.Add(this.GeneralGameTextBox);
+            this.GeneralFlowLayout.Controls.Add(this.GeneralGameBrowseButton);
+            this.GeneralFlowLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.GeneralFlowLayout.Location = new System.Drawing.Point(3, 3);
+            this.GeneralFlowLayout.Name = "GeneralFlowLayout";
+            this.GeneralFlowLayout.Padding = new System.Windows.Forms.Padding(0, 5, 0, 5);
+            this.GeneralFlowLayout.Size = new System.Drawing.Size(334, 376);
+            this.GeneralFlowLayout.TabIndex = 0;
             // 
             // GeneralGameLabel
             // 
@@ -147,6 +150,15 @@
             this.MainOkButton.UseVisualStyleBackColor = true;
             this.MainOkButton.Click += new System.EventHandler(this.MainOkButton_Click);
             // 
+            // AboutFlowLayout
+            // 
+            this.AboutFlowLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.AboutFlowLayout.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.AboutFlowLayout.Location = new System.Drawing.Point(3, 3);
+            this.AboutFlowLayout.Name = "AboutFlowLayout";
+            this.AboutFlowLayout.Size = new System.Drawing.Size(334, 376);
+            this.AboutFlowLayout.TabIndex = 0;
+            // 
             // SettingsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -155,7 +167,7 @@
             this.Controls.Add(this.MainOkButton);
             this.Controls.Add(this.MainCancelButton);
             this.Controls.Add(this.MainPlayButton);
-            this.Controls.Add(this.settingsTabControl);
+            this.Controls.Add(this.MainTabControl);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -163,25 +175,27 @@
             this.Padding = new System.Windows.Forms.Padding(5);
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "ROFL Player Settings";
-            this.settingsTabControl.ResumeLayout(false);
-            this.MainTab.ResumeLayout(false);
-            this.flowLayoutPanel1.ResumeLayout(false);
-            this.flowLayoutPanel1.PerformLayout();
+            this.MainTabControl.ResumeLayout(false);
+            this.GeneralTab.ResumeLayout(false);
+            this.AboutTab.ResumeLayout(false);
+            this.GeneralFlowLayout.ResumeLayout(false);
+            this.GeneralFlowLayout.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TabControl settingsTabControl;
-        private System.Windows.Forms.TabPage MainTab;
+        private System.Windows.Forms.TabControl MainTabControl;
+        private System.Windows.Forms.TabPage GeneralTab;
         private System.Windows.Forms.TabPage AboutTab;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel GeneralFlowLayout;
         private System.Windows.Forms.Label GeneralGameLabel;
         private System.Windows.Forms.TextBox GeneralGameTextBox;
         private System.Windows.Forms.Button GeneralGameBrowseButton;
         private System.Windows.Forms.Button MainPlayButton;
         private System.Windows.Forms.Button MainCancelButton;
         private System.Windows.Forms.Button MainOkButton;
+        private System.Windows.Forms.FlowLayoutPanel AboutFlowLayout;
     }
 }
