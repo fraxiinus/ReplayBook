f38546835068593e103d0d4ac9fd9adafc4355ed
Andrew Lee <andrew1421lee@gmail.com>
2018-06-20 00:03:23 -0400

    all form logic should go in here

---
+++ ROFLPlayer/Lib/MainWindowManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ROFLPlayer.Lib
+{
+    public class MainWindowManager
+    {
+        public static void Load(Form form)
+        {
+            form.Controls.Find("GeneralGameTextBox", true)[0].Text = RoflSettings.Default.LoLExecLocation;
+        }
+
+        public static void CloseWindow(Form form)
+        {
+            // Save and Exit form
+            RoflSettings.Default.LoLExecLocation = form.Controls.Find("GeneralGameTextBox", true)[0].Text;
+            RoflSettings.Default.Save();
+            Environment.Exit(1);
+        }
+    }
+}
