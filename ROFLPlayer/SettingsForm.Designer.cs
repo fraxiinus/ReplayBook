7c4adecf34ed54e72576f1645951968616098bdc
Andrew <andrew1421lee@gmail.com>
2019-03-19 03:03:51 -0400

    new version number

--- ROFLPlayer/SettingsForm.Designer.cs
+++ ROFLPlayer/SettingsForm.Designer.cs
@@ -496,7 +496,7 @@
             this.AboutVersionLabel.Name = "AboutVersionLabel";
             this.AboutVersionLabel.Size = new System.Drawing.Size(328, 23);
             this.AboutVersionLabel.TabIndex = 3;
-            this.AboutVersionLabel.Text = "Beta Release 5";
+            this.AboutVersionLabel.Text = "Beta Release 6";
             this.AboutVersionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // AboutGithubButton
