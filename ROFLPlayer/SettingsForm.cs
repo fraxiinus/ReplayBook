e4b510f37fdf956b149a00405842e120c026c08f
Andrew <andrew1421lee@gmail.com>
2019-04-27 15:45:41 -0400

    form no longer crashes if no executable is selected

--- ROFLPlayer/SettingsForm.cs
+++ ROFLPlayer/SettingsForm.cs
@@ -33,39 +33,6 @@ namespace ROFLPlayer
 
         private void SettingsForm_Load(object sender, EventArgs e)
         {
-            /// THIS BLOCK OF CODE IS FOR AUTOMATICALLY UPDATING THE EXECUTABLE LOCATION
-            // TODO MOVE THIS SOMEWHERE ELSE LOL
-            /*
-            if (string.IsNullOrEmpty(RoflSettings.Default.LoLExecLocation) || !File.Exists(RoflSettings.Default.LoLExecLocation))
-            {
-                if (GameLocator.FindLeagueInstallPath(out string path))
-                {
-                    try
-                    {
-                        var execPath = GameLocator.FindLeagueExecutable(path);
-                        RoflSettings.Default.LoLExecLocation = path;
-                        this.GeneralGameTextBox.Text = execPath;
-
-                        MessageBox.Show("Automatically detected League of Legends install!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show("Could not find League of Legends executable, please select the game executable (League of Legends.exe)\n\n" + ex.Message, "Error finding game executable", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        GeneralGameBrowseButton_Click(this, new EventArgs());
-                    }
-                }
-                else
-                {
-                    MessageBox.Show("Could not find League of Legends install location, please select the game launcher (LeagueClient.exe)\n\n" + path + ".", "Error finding install path", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    GeneralInstallBrowseButton_Click(this, new EventArgs());
-                }
-            }
-            else
-            {
-                this.GeneralGameTextBox.Text = RoflSettings.Default.LoLExecLocation;
-            } */
-            ///
-
             // Set version text in about tab
             this.AboutVersionLabel.Text = RoflSettings.Default.VersionString;
 
@@ -120,7 +87,10 @@ namespace ROFLPlayer
         private void MainOkButton_Click(object sender, EventArgs e)
         {
             // Save selected default exec
-            ExecsManager.SetDefaultExecByName(this.GeneralGameComboBox.SelectedItem.ToString());
+            if(this.GeneralGameComboBox.SelectedItem != null)
+            {
+                ExecsManager.SetDefaultExecByName(this.GeneralGameComboBox.SelectedItem.ToString());
+            }
 
             // Save double click launch option
             RoflSettings.Default.StartupMode = this.GeneralLaunchComboBox.SelectedIndex;
@@ -143,75 +113,6 @@ namespace ROFLPlayer
         {
             Environment.Exit(1);
         }
-
-        /*
-        private void GeneralGameBrowseButton_Click(object sender, EventArgs e)
-        {
-            var dialog = new OpenFileDialog
-            {
-                Filter = "League of Legends.exe (*.exe)|*.exe",
-                Multiselect = false,
-                Title = "Select League of Legends executable"
-            };
-            while (dialog.ShowDialog() == DialogResult.OK)
-            {
-                var filepath = dialog.FileName;
-                if (string.IsNullOrEmpty(filepath))
-                {
-                    return;
-                }
-
-                if (GameLocator.CheckLeagueExecutable(filepath))
-                {
-                    this.GeneralGameTextBox.Text = filepath;
-                    return;
-                }
-                else
-                {
-                    MessageBox.Show("Invalid League executable", "Invalid File", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
-        }
-        */
-
-        /*
-        private void GeneralInstallBrowseButton_Click(object sender, EventArgs e)
-        {
-            var dialog = new OpenFileDialog
-            {
-                Filter = "LeagueClient.exe (*.exe)|*.exe",
-                Multiselect = false,
-                Title = "Select League of Legends client"
-            };
-            while (dialog.ShowDialog() == DialogResult.OK)
-            {
-                var filepath = dialog.FileName;
-                if (string.IsNullOrEmpty(filepath))
-                {
-                    return;
-                }
-
-                try
-                {
-                    var path = GameLocator.FindLeagueExecutable(Path.GetDirectoryName(filepath));
-                    RoflSettings.Default.LoLExecLocation = path;
-                    this.GeneralGameTextBox.Text = path;
-                    return;
-                }
-                catch(Exception ex)
-                {
-                    MessageBox.Show("Could not find League of Legends executable, please try again\n\n" + ex.Message, "Error finding game executable", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
-        }
-        */
-
-        /*
-        private void GeneralGameClearButton_Click(object sender, EventArgs e)
-        {
-            this.GeneralGameTextBox.Text = string.Empty;
-        }
-        */
 
         /// <summary>
         /// Actions for github button
