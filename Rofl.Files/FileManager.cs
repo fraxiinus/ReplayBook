613438c9d3bfebec8a6723d5b880bd4cd6a86e93
Andrew <andrew1421lee@gmail.com>
2019-08-10 22:03:45 -0400

    files project

---
+++ Rofl.Files/FileManager.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace Rofl.Files
+{
+    public class FileManager
+    {
+
+        private IConfiguration _config;
+
+        private List<string> folders;
+
+        public FileManager(IConfiguration config)
+        {
+            _config = config;
+
+            folders = _config.GetSection("file-manager:folders").Get<List<string>>();
+        }
+    }
+}
