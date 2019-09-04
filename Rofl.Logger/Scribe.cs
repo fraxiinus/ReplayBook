5840d75d78ddf681e7211ea85dd9e09d92599539
Andrew <andrew1421lee@gmail.com>
2019-09-04 19:01:14 -0400

    log tweaks

--- Rofl.Logger/Scribe.cs
+++ Rofl.Logger/Scribe.cs
@@ -5,13 +5,16 @@ using System.IO;
 
 namespace Rofl.Logger
 {
+    /// <summary>
+    /// Logs messages and errors.
+    /// Will dump logs in "logs" folder when an error is logged.
+    /// </summary>
     public class Scribe
     {
         private List<LogEntry> _entryList;
         private bool _errorFlag;
 
         public string OutputDirectory { get; set; } = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs");
-
 
         public Scribe()
         {
@@ -26,18 +29,17 @@ namespace Rofl.Logger
 
         public void WriteToFile()
         {
+            if (!_errorFlag) { return; } // No errors occured, skip
+
             string outputFileName = Path.Combine(OutputDirectory, $"Error_{ DateTime.Now.ToString("yyyyMMdd_HHmm")}.log");
-            if (_errorFlag)
+            string logOutput = "";
+
+            foreach (LogEntry entry in _entryList)
             {
-                string logOutput = "";
-
-                foreach (LogEntry entry in _entryList)
-                {
-                    logOutput += $"{entry.Timestamp} | {entry.ClassName} -> {entry.MethodName} | {entry.Level} | {entry.Message}\n";
-                }
-
-                File.WriteAllText(outputFileName, logOutput);
+                logOutput += $"{entry.Timestamp} | {entry.ClassName} -> {entry.MethodName} | {entry.Level} | {entry.Message}\n";
             }
+
+            File.WriteAllText(outputFileName, logOutput);
         }
 
         public void Debug(string className, string message,
@@ -97,7 +99,7 @@ namespace Rofl.Logger
                 Timestamp = DateTime.Now
             };
 
-            _errorFlag = true;
+            _errorFlag = true;  // Set the error flag, logs will be created
             _entryList.Add(newEntry);
         }
     }
