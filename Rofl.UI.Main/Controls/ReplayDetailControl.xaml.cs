afcd568c59bfe36409a43e46784b1e52d3054f69
Andrew Lee <andrew1421lee@gmail.com>
2020-01-17 23:34:09 -0500

    enable context menu in detail view as well

--- Rofl.UI.Main/Controls/ReplayDetailControl.xaml.cs
+++ Rofl.UI.Main/Controls/ReplayDetailControl.xaml.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -45,6 +46,26 @@ namespace Rofl.UI.Main.Controls
         private void StatsScrollViewer_Loaded(object sender, RoutedEventArgs e)
         {
             StatsScrollViewer.ScrollToVerticalOffset(0);
+        }
+
+        private void MoreButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(sender is Button moreButton)) { return; }
+
+            // Get the button and menu
+            ContextMenu contextMenu = moreButton.ContextMenu;
+            // Set placement and open
+            contextMenu.PlacementTarget = moreButton;
+            contextMenu.Placement = PlacementMode.Bottom;
+            contextMenu.IsOpen = true;
+        }
+
+        private void OpenContainingFolder_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(Window.GetWindow(this).DataContext is MainWindowViewModel context)) { return; }
+            if (!(this.DataContext is ReplayDetailModel replay)) { return; }
+
+            context.OpenReplayContainingFolder(replay.PreviewModel.Location);
         }
     }
 }
