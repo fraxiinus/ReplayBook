d1f0e259cf5cc8d6d1b2f745b533521263f9f122
A Lee <andrew1421lee@gmail.com>
2020-05-24 14:26:41 -0400

    disable maximize executable select window, style fixes

--- Rofl.UI.Main/Views/ExecutableSelectWindow.xaml.cs
+++ Rofl.UI.Main/Views/ExecutableSelectWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using Rofl.Executables.Models;
+﻿using System;
+using Rofl.Executables.Models;
 using System.Windows;
+using System.Windows.Interop;
 
 namespace Rofl.UI.Main.Views
 {
@@ -20,6 +22,20 @@ namespace Rofl.UI.Main.Views
             this.MinWidth = this.ActualWidth;
             this.MinHeight = this.ActualHeight;
             this.MaxHeight = this.ActualHeight;
+        }
+
+        private void ExecutableSelectWindow_OnSourceInitialized(object sender, EventArgs e)
+        {
+            // Change window style
+            var GWL_STYLE = -16;
+            // Maximize box flag
+            var WS_MAXIMIZEBOX = 0x10000;
+
+            var windowHandle = new WindowInteropHelper((Window)sender).Handle;
+            var value = NativeMethods.GetWindowLong(windowHandle, GWL_STYLE);
+
+            // Flip maximize box flag
+            _ = NativeMethods.SetWindowLong(windowHandle, GWL_STYLE, (int) (value & ~WS_MAXIMIZEBOX));
         }
 
         private void OKButton_Click(object sender, RoutedEventArgs e)
