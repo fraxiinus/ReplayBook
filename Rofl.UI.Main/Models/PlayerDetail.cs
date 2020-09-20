bb2bd62525f201053eac3388b393cc7f86e6f24e
A Lee <andrew1421lee@gmail.com>
2020-09-20 14:46:16 -0400

    fix null access bug

--- Rofl.UI.Main/Models/PlayerDetail.cs
+++ Rofl.UI.Main/Models/PlayerDetail.cs
@@ -97,7 +97,7 @@ namespace Rofl.UI.Main.Models
             Ping = player.PING.ToInt();
             // Only capitalize first letter of position name JUNGLE -> Jungle
 #pragma warning disable CA1308 // Normalize strings to uppercase
-            IndividualPosition = player.INDIVIDUAL_POSITION[0] + player.INDIVIDUAL_POSITION.Substring(1).ToLowerInvariant();
+            IndividualPosition = player.INDIVIDUAL_POSITION == null ? "N/A" : player.INDIVIDUAL_POSITION[0] + player.INDIVIDUAL_POSITION.Substring(1).ToLowerInvariant();
 #pragma warning restore CA1308 // Normalize strings to uppercase
             TeamEarlySurrendered = Convert.ToBoolean(player.TEAM_EARLY_SURRENDERED.ToInt());
             TimeOfLastDisconnect = player.TIME_OF_FROM_LAST_DISCONNECT.ToInt();
