383069b82c4d76cdd3476c83af62468e3ff7a2ef
A <ash1113lee@gmail.com>
2021-02-13 00:04:24 -0500

    New Logo with Readme changes (#47)
    * new readme
    
    * update icon in program
    
    * consistent program name

--- Rofl.Requests/Utilities/DownloadClient.cs
+++ Rofl.Requests/Utilities/DownloadClient.cs
@@ -19,7 +19,7 @@ namespace Rofl.Requests.Utilities
         private readonly RiZhi _log;
         private readonly HttpClient _httpClient;
 
-        private const string UserAgent = @"ReplayBook/PR9.3 (+https://github.com/fraxiinus/ReplayBook)";
+        private const string UserAgent = @"ReplayBook/R1.0 (+https://github.com/fraxiinus/ReplayBook)";
 
         private string LatestDataDragonVersion = null;
 
