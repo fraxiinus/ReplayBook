b31a924f35eb96cb91584f32b4f7f06ca71ffccf
Fraxiinus <8365233+fraxiinus@users.noreply.github.com>
2021-07-29 23:15:31 -0400

    Features/prepare1 3 release (#82)
    * sort locale codes
    
    * bump version number

--- Rofl.UI.Main/Properties/AssemblyInfo.cs
+++ Rofl.UI.Main/Properties/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Windows;
 [assembly: AssemblyTitle("ReplayBook")]
 [assembly: AssemblyDescription("Replay management tool for League of Legends")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Etirps")]
+[assembly: AssemblyCompany("Fraxiinus")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.1.0")]
-[assembly: AssemblyFileVersion("1.2.1.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
