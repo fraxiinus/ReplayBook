6c72b596ad59a022f22a0d15f336575846097ccf
Fraxiinus <8365233+fraxiinus@users.noreply.github.com>
2021-10-31 14:02:39 -0400

    add format choices (#109)

--- scripts/compare-strings.py
+++ scripts/compare-strings.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import json
 import xml.etree.ElementTree as ET
 
 def dir_path(string):
@@ -8,9 +9,10 @@ def dir_path(string):
     else:
         raise NotADirectoryError(string)
 
-parser = argparse.ArgumentParser(description="Copies missing strings from a main resource file to others")
+parser = argparse.ArgumentParser(prog="compare-strings.py", description="Compares strings from a main resource file to others")
 parser.add_argument("directory", metavar="<resource directory>", type=dir_path, help="directory where all resource files are")
 parser.add_argument("main", metavar="<reference file>", help="file to compare all other files to")
+parser.add_argument("-format", metavar="<choice>", choices=["summary", "json", "csv"], default="summary", help="result format choices: 'summary', 'json', 'csv' (default: 'summary')")
 
 args = parser.parse_args()
 
@@ -37,7 +39,7 @@ for fileName in os.listdir(args.directory):
     if resFile == args.main:
         continue
 
-    print(f"--- reading {fileName} ---")
+    print(f"reading {fileName}...")
     results[fileName] = []
 
     # start comparing strings
@@ -51,26 +53,45 @@ for fileName in os.listdir(args.directory):
         if resRoot.find(xpath) is None:
             results[fileName].append(stringKey)
             #print(f"{stringKey} is missing from {fileName}")
-
-csvLines = []
-# construct table output
-firstLine = "key, " + os.path.basename(args.main)
-for fileName in results:
-    firstLine += ", " + fileName
-csvLines.append(firstLine)
-
-for refString in allStringRes:
-    key = refString.attrib['{http://schemas.microsoft.com/winfx/2006/xaml}Key']
-    newLine = key + ", ✔"
-
-    # loop over all files in results
+print("- - - - - - - - - - - - - - -")
+if args.format == "summary":
+    # output percentages
+    print("")
+    print("Current strings status:")
+    totalStringCount = len(allStringRes)
+    columnKey = os.path.basename(args.main)
+    columnData = str(totalStringCount) + "/" + str(totalStringCount)
     for resFile in results:
-        missingStrings = results[resFile]
-        if key in missingStrings:
-            newLine += ", ❌"
-        else:
-            newLine += ", ✔"
-    csvLines.append(newLine)
+        missingCount = len(results[resFile])
+        columnKey += "\t" + resFile
+        columnData += "\t" + str(totalStringCount - missingCount) + "/" + str(totalStringCount)
+    print(columnKey)
+    print(columnData)
+elif args.format == "json":
+    # output json
+    json_object = json.dumps(results, indent=4)
+    print(json_object)
+elif args.format == "csv":
+    # output csv
+    csvLines = []
+    # construct table output
+    firstLine = "key, " + os.path.basename(args.main)
+    for fileName in results:
+        firstLine += ", " + fileName
+    csvLines.append(firstLine)
 
-for line in csvLines:
-    print(line)
+    for refString in allStringRes:
+        key = refString.attrib['{http://schemas.microsoft.com/winfx/2006/xaml}Key']
+        newLine = key + ", ✔"
+
+        # loop over all files in results
+        for resFile in results:
+            missingStrings = results[resFile]
+            if key in missingStrings:
+                newLine += ", ❌"
+            else:
+                newLine += ", ✔"
+        csvLines.append(newLine)
+
+    for line in csvLines:
+        print(line)
