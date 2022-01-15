953befaf9cd0eed1f1c99d40a1ee0ebecf4f1fa7
A. Lee <8365233+fraxiinus@users.noreply.github.com>
2022-01-15 18:32:35 -0500

    search now "works"

--- src/Rofl.Files/Models/FileResult.cs
+++ src/Rofl.Files/Models/FileResult.cs
@@ -1,5 +1,6 @@
 ﻿using Rofl.Reader.Models;
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 
@@ -17,17 +18,18 @@ namespace Rofl.Files.Models
 
             Id = FileInfo.Path;
 
-            SearchKeywords = $"{FileInfo.Name.ToUpper(CultureInfo.InvariantCulture)};" +
-                $"{ReplayFile.AlternativeName.ToUpper(CultureInfo.InvariantCulture)};" +
-                $"{string.Join("|", ReplayFile.Players.Select(x => x.NAME.ToUpper(CultureInfo.InvariantCulture)))};" +
-                $"{string.Join("|", ReplayFile.Players.Select(x => x.SKIN.ToUpper(CultureInfo.InvariantCulture)))}";
+            SearchKeywords = new List<string>
+            {
+                FileInfo.Name.ToUpper(CultureInfo.InvariantCulture),
+                ReplayFile.AlternativeName.ToUpper(CultureInfo.InvariantCulture)
+            };
+            SearchKeywords.AddRange(ReplayFile.Players.Select(x => x.NAME.ToUpper(CultureInfo.InvariantCulture)));
+            SearchKeywords.AddRange(ReplayFile.Players.Select(x => x.SKIN.ToUpper(CultureInfo.InvariantCulture)));
 
             FileName = FileInfo.Name;
             AlternativeName = replayFile.AlternativeName;
             FileSizeBytes = FileInfo.FileSizeBytes;
             FileCreationTime = FileInfo.CreationTime;
-            //PlayerNames = string.Join("|", ReplayFile.Players.Select(x => x.NAME.ToUpper(CultureInfo.InvariantCulture)));
-            //ChampionNames = string.Join("|", ReplayFile.Players.Select(x => x.SKIN.ToUpper(CultureInfo.InvariantCulture)));
         }
         
         public ReplayFileInfo FileInfo { get; set; }
@@ -40,24 +42,12 @@ namespace Rofl.Files.Models
 
         // The following fields are used to allow for fast indexing
         // Placing them on the root level object makes creating indexes very easy and clear.
-
-        /// <summary>
-        /// Contains all search keywords, delimited by semicolon
-        /// Only field used by search
-        /// </summary>
-        public string SearchKeywords { get; set; }
+        public List<string> SearchKeywords { get; set; }
 
         // The following fields are only used for sorting
         public string FileName { get; set; }
         public long FileSizeBytes { get; set; }
         public DateTime FileCreationTime { get; set; }
         public string AlternativeName { get; set; }
-
-        ///// <summary>
-        ///// User assigned name alternative
-        ///// </summary>
-        //
-        //public string PlayerNames { get; set; }
-        //public string ChampionNames { get; set; }
     }
 }
