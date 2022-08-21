2a52ec5f9b910cc80769b8387682738297b7ce31
Fraxiinus <8365233+fraxiinus@users.noreply.github.com>
2022-08-21 19:58:01 -0400

    use download dialog in all situations, add retry

--- src/UI.Main/Views/StaticDataAddDialog.xaml.cs
+++ src/UI.Main/Views/StaticDataAddDialog.xaml.cs
@@ -1,11 +1,6 @@
-﻿using Fraxiinus.ReplayBook.Configuration.Models;
-using Fraxiinus.ReplayBook.StaticData;
-using Fraxiinus.ReplayBook.StaticData.Extensions;
-using Fraxiinus.ReplayBook.StaticData.Models;
-using Fraxiinus.ReplayBook.UI.Main.Utilities;
+﻿using Fraxiinus.ReplayBook.StaticData;
 using ModernWpf.Controls;
 using System;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace Fraxiinus.ReplayBook.UI.Main.Views
@@ -18,6 +13,13 @@ namespace Fraxiinus.ReplayBook.UI.Main.Views
         public StaticDataAddDialog()
         {
             InitializeComponent();
+
+            PatchComboBox.SelectedIndex = 0;
+        }
+
+        public string SelectedPatch
+        {
+            get => PatchComboBox.SelectedItem.ToString();
         }
 
         private StaticDataManager Context
@@ -36,43 +38,6 @@ namespace Fraxiinus.ReplayBook.UI.Main.Views
 
             RefreshPatchesButton.IsEnabled = true;
             PatchComboBox.IsEnabled = true;
-        }
-
-        private async void DownloadButton_Click(object sender, RoutedEventArgs e)
-        {
-            DownloadButton.IsEnabled = false;
-            CancelButton.IsEnabled = false;
-            PatchComboBox.IsEnabled = false;
-            RefreshPatchesButton.IsEnabled = false;
-
-            ProgressBarPanel.Visibility = Visibility.Visible;
-
-            var patch = PatchComboBox.SelectedItem.ToString();
-            var progressBar = (ProgressBarPanel.Children[0] as ProgressBar);
-
-            await Context.DownloadProperties(patch,
-                    StaticDataType.Rune | StaticDataType.Item | StaticDataType.Champion,
-                    LanguageHelper.CurrentLanguage.GetRiotRegionCode());
-            
-            progressBar.IsIndeterminate = false;
-            progressBar.Value = 33;
-
-            await Context.DownloadImages(patch, StaticDataType.Item | StaticDataType.Champion);
-
-            progressBar.Value = 66;
-
-            await Context.DownloadRuneImages(patch, LanguageHelper.CurrentLanguage.GetRiotRegionCode());
-
-            progressBar.Value = 100;
-
-            await Task.Delay(1000);
-
-            Hide();
-        }
-
-        private void CancelButton_Click(object sender, RoutedEventArgs e)
-        {
-            Hide();
         }
     }
 }
